c2b44e1c298ac705624a597dccd4c1f3027c5f18
Marcelo Prates <mpratesjunior@gmail.com>
2023-01-11 20:20:57 -0300

    feat: cria tableas

---
+++ libs/database/src/migrations/20230111225653-create-table-conta.js
@@ -0,0 +1,52 @@
+/* eslint-disable camelcase */
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('contas', {
+      id: {
+        primaryKey: true,
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+      },
+      portador_id: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        references: {
+          model: {
+            tableName: 'portadores',
+          },
+          key: 'document'
+        }
+      },
+      agencia: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      conta: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      },
+      saldo: {
+        type: Sequelize.DECIMAL,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      deleted_at: {
+        type: Sequelize.DATE,
+      },
+    });
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('contas');
+  },
+};
